0fd5f8a21551defe42469f69fca9be52146022a7
shashankpulijala <shashankpulijala@gmail.com>
2024-03-31 22:50:14 +0100

    Added ingestion, and training pipeline

---
+++ src/utils.py
@@ -0,0 +1,21 @@
+import pickle
+import os
+import sys
+# import dill
+import pandas as pd
+import numpy as np
+from src.exception import CustomException
+from src.logger import logging
+
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
