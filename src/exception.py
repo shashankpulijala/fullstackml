6d1231111fcf6dd81b8631f966ee322841b14700
shashankpulijala <shashankpulijala@gmail.com>
2024-03-31 14:26:00 +0100

    exception error fixed

--- src/exception.py
+++ src/exception.py
@@ -1,7 +1,7 @@
-import sys, logging
-
+import sys
+from src.logger import logging
 def error_message_details(error, error_detail:sys):
-    _, _,exc_tb= error_detail.ex_info()
+    _, _,exc_tb= error_detail.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
     error_message="Error occured in Python script name[{0}] \
                 line number [{1}] error message [{2}]".format(file_name,
@@ -15,4 +15,3 @@ class CustomException(Exception) :
                                                    error_detail=error_detail)
     def __str__(self):
         return self.error_message
-        
